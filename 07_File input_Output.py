3354edeccd380f9e39557bacf1ef8b8da06ac32b
Rushikesh Bhosale <rushibhosale3303@gmail.com>
2026-07-28 15:36:31 +0530

    Add File input output

---
+++ 07_File input_Output.py
@@ -0,0 +1,35 @@
+# open , read and close file 
+
+# f = open("file_name ","mode") 
+
+# data = f.read()
+
+# f.close()
+
+#  mode = 
+
+# r: read mode (default)
+# w: write mode
+# x : create a new file 
+# a : open for writing , appending to the end of the file if it exists
+# b : binary mode 
+# t : text mode (default)
+# + : open a disk file for updating (reading and writing)
+
+# read 
+
+# data = f.read()   read entire file
+
+# data = f.readline  read one line at a time
+
+# exp 
+
+# f = open("demo.txt","r")
+#  line1 = f.readline()
+#  print(line1)
+
+# line2 = f.readline()
+# print(line2)
+
+# f.close
+
