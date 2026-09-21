5f875dbe825b5a03481510ab5d7bf7fc3e04f812
Rushikesh Bhosale <rushibhosale3303@gmail.com>
2026-09-21 12:30:11 +0530

    Add files via upload

---
+++ 08_Class And Objects.py
@@ -0,0 +1,18 @@
+# CLASS AND OBJECT:-
+
+class Students:
+   name = "Rushi Bhosale"   # this is class 
+
+s1 = Students()
+print(s1.name)         # this is objects
+
+s2 = Students()
+print(s2.name)
+
+
+class Car:
+    color = "blue"
+    brand = "BMW"
+s1 = Car()
+print(s1.color)
+print(s1.brand)
